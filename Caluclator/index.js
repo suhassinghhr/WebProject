d3482b031943cf5915dfd2a09fd64996779a0cfe
SUHAS SINGH H R <suhassinghhr10@gmail.com>
2025-11-12 13:08:09 +0530

    Add files via upload

---
+++ Caluclator/index.js
@@ -0,0 +1,14 @@
+function display(value){
+ document.getElementById('result').value+=value
+}
+
+function clearScreen(){
+    document.getElementById('result').value=""
+}
+
+
+function calculate(){
+    let p=document.getElementById('result').value
+    let q=eval(p)
+    document.getElementById('result').value=q
+}
